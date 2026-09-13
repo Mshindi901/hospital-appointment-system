48469c93c04cf8f08b27d3d44e029a382b383f4b
emmanuel mshindi <mshindi@kali.mshindi>
2026-09-13 16:36:37 +0300

    debugged

--- src/patients/routes.js
+++ src/patients/routes.js
@@ -12,7 +12,7 @@ const router = express.Router();
 
 router.post('/patient', authenticate, authorize('manager'), newRecord);
 router.get('/patient/hospitals/:id', authenticate, authorize('manager', 'admin'), getRecordByHospitals);
-router.get('/patient/:id', authenticate, authorize('manager', 'admin'), getRecordById);
+router.get('/patient/:id', authenticate, authorize('manager', 'admin', 'doctor'), getRecordById);
 router.put('/patient/:id',authenticate, authorize('manager'), updateRecord);
 router.delete('/patient/:id', authenticate, authorize('manager'), deleteRecord);
 
