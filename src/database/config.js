671dcedd25a6a922639b40ab3af8e4c810806fd7
emmanuel mshindi <mshindi@kali.mshindi>
2026-09-13 13:46:40 +0300

    added render db internal url for db services in prod

--- src/database/config.js
+++ src/database/config.js
@@ -2,7 +2,7 @@ import { Sequelize } from "sequelize";
 import dotenv from 'dotenv';
 dotenv.config();
 
-const sequelize = new Sequelize('hospital', 'postgres', process.env.DB_PASSWORD, {
+const sequelize = new Sequelize('postgresql://caresync_app_db_user:CUOCL2SX9Q1HGobulr21lEUIwbBXIdh6@dpg-daj7thek1f9s73ck14hg-a/caresync_app_db', {
     host: 'localhost',
     dialect: 'postgres',
 });
